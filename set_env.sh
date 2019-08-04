8368d26caccaa964598d36a5d1b2bc9ed7cf39c8
shanakaprageeth <shanakaprageeth@gmail.com>
2019-08-04 20:35:10 +0900

    script to install tools

---
+++ set_env.sh
@@ -0,0 +1,16 @@
+#workon ml_test_env
+sudo apt-get install python3 python3-pip
+TOOL=pip3
+${TOOL} install numpy
+${TOOL} install scipy
+${TOOL} install imutils
+${TOOL} install scikit-learn
+${TOOL} install matplotlib
+${TOOL} install tensorflow
+${TOOL} install pillow
+${TOOL} install h5py
+${TOOL} install anaconda
+${TOOL} install keras
+${TOOL} install virtualenv virtualenvwrapper
+${TOOL} install opencv-python
+
